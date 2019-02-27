246a204869479db60c402f2738c7f1231171bc86
Alan Huber <aghuber11@gmail.com>
2019-02-27 13:21:36 -0800

    Toggle content

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
 var toggle = document.getElementsByClassName("toggle");
 
-var toggleContent = function() {
-  console.log(this);
+const toggleContent = function() {
+  var clickTarget = this.getAttribute("data");
+  slideCheck(clickTarget);
 };
+
+function slideCheck(clickTarget) {
+  var code = document.getElementById('code');
+  var design = document.getElementById('design');
+  var contact = document.getElementById('contact');
+
+  if (clickTarget == "code") {
+    code.classList.toggle("active");
+  }
+  else if (clickTarget == "design") {
+    design.classList.toggle("active");
+  }
+  else if (clickTarget == "contact") {
+    contact.classList.toggle("active"); 
+  }
+  else {
+    //do nothing
+  }
+}
 
 for (var i = 0; i < toggle.length; i++) {
     toggle[i].addEventListener('click', toggleContent, false);
